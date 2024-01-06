20e7842ae05780facb15c7d628b08c61ceb26704
Dominik Klimpera <58090210+Hypnotikon@users.noreply.github.com>
2024-01-06 10:05:09 -0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,48 @@
+import keyboard
+import time
+import pyautogui
+
+
+def mouse(interval):   #every 3 minute eat one chicken
+    if interval % 1800 == 0:
+        pyautogui.mouseDown(button='right')
+        for i in range(30):
+            keyboard.press('w')
+            time.sleep(0.1)
+        pyautogui.mouseUp(button='right')
+
+
+
+def type_w_for_duration(duration_seconds):
+    interval = 1
+    print(f"i for start, i again for pause, o for end")
+
+    while True:
+        if keyboard.is_pressed('o'):  # Check if 'o' key is pressed to exit before starting
+            print("end")
+            return
+
+        if keyboard.is_pressed('i'):
+            time.sleep(0.3)  # Wait for 'i' key press to start
+            print(f"start")
+            start_time = time.time()
+
+            while (time.time() - start_time) < duration_seconds:
+                if keyboard.is_pressed('i'):  # Check if 'k' key is pressed to stop
+                    time.sleep(0.3)
+                    print("pause")
+                    break
+
+                if keyboard.is_pressed('o'):  # Check if 'o' key is pressed to exit
+                    print("Exiting...")
+                    return
+
+                keyboard.press('w')
+                time.sleep(0.1)  # Adjust this delay if needed
+                keyboard.release('w')
+                interval = interval + 1
+                mouse(interval)
+
+if __name__ == "__main__":
+    duration = 999999999999  # Duration in seconds to type 'W'
+    type_w_for_duration(duration)
